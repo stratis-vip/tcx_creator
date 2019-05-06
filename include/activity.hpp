048a8978ccb6ec30715f68c161822912ce67c2cb
stratis <stratis.vip@gmail.com>
2019-05-06 18:52:10 +0300

    minor changes

--- include/activity.hpp
+++ include/activity.hpp
@@ -1,62 +1,49 @@
 #ifndef activity_hpp
 #define activity_hpp
 
-#include "pugixml.hpp"
 #include "infostructure.hpp"
 #include "options.hpp"
+#include "pugixml.hpp"
 
 using namespace pugi;
 
 const static size_t LAP_RUNNING = 1000;
 
-class Activity
-{
+class Activity {
 public:
   Activity() = default;
-  void loadInfo(Info infoStruct) const
-  {
+  void loadInfo(Info infoStruct) const {
     *_act = _node->append_child("Activity");
     addSportAttribute(infoStruct.sport.c_str());
     addIdTag(infoStruct.id);
     const size_t distance = infoStruct.distance;
     const size_t lapsEvery = infoStruct.lapsEvery;
     const size_t laps = lapsEvery == 0 ? 1 : distance / lapsEvery;
-    for (auto _ = 0; _ != laps; _++)
-    {
+    for (size_t _ = 0; _ != laps; _++) {
       xml_node lap = _act->append_child("Lap");
       lap.append_attribute("StartTime");
     }
     _act->append_child("Creator");
   }
-  Activity(Info infoStruct) : _id(infoStruct.id.c_str()), _sport(infoStruct.sport.c_str())
-  {
+  Activity(Info infoStruct)
+      : _id(infoStruct.id.c_str()), _sport(infoStruct.sport.c_str()) {
     loadInfo(infoStruct);
   }
 
-  ~Activity()
-  {
+  ~Activity() {
     delete _act;
     delete _node;
   }
 
-  const xml_node getNode()
-  {
-    return *_act;
-  }
+  const xml_node getNode() { return *_act; }
 
-  void setSportType(const char *sportType) const
-  {
+  void setSportType(const char *sportType) const {
     _act->attribute("Sport") = sportType;
   }
 
-  const char *getSportType() const
-  {
-    return _act->attribute("Sport").value();
-  }
+  const char *getSportType() const { return _act->attribute("Sport").value(); }
 
-  const char *getId() const {
-    return _act->child("Id").first_child().value();
-  }
+  const char *getId() const { return _act->child("Id").first_child().value(); }
 
 private:
   xml_node *_node = new xml_document();
@@ -65,13 +52,11 @@ private:
   const char *_id;
   const char *_sport;
 
-  void addSportAttribute(const std::string &sport) const
-  {
+  void addSportAttribute(const std::string &sport) const {
     _act->append_attribute("Sport") = sport.c_str();
   }
 
-  void addIdTag(const std::string &id) const
-  {
+  void addIdTag(const std::string &id) const {
     auto idTag = _act->append_child("Id");
     auto text = idTag.append_child(node_pcdata);
     text.set_name("Id");
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,24 @@
-#include <iostream>
-#include "tcxobject.hpp"
-#include "options.hpp"
 #include "infostructure.hpp"
+#include "options.hpp"
+#include "tcxobject.hpp"
+#include <iostream>
 
-int main(int argc, char *argv[])
-{
-    Info of;
-    of.id = "1";
-    of.distance = 12345;
-    of.lapsEvery = 1000;
-     TcxObject tcx{"/Users/stratis/Desktop/dev/c++/tcx_creator/options.json"};
-     //tcx.addActivity("2017-10-29T07:11:03.000Z","Cycling");
+int main(int argc, char *argv[]) {
+  Info of;
+  of.id = "1";
+  of.distance = 12345;
+  of.lapsEvery = 1000;
+  // TcxObject tcx{"/Users/stratis/Desktop/dev/c++/tcx_creator/options.json"};
+  TcxObject tcx{"/home/stratis/dev/c++/tcx_creator/options.json"};
+  // tcx.addActivity("2017-10-29T07:11:03.000Z","Cycling");
 
-    // TcxObject tcx{false};
-    std::cout << tcx.print() << std::endl;
-    //Options opt("/Users/stratis/Desktop/dev/c++/tcx_creator/options.json");
-    if (!tcx.isEmpty())
-    {
-        
-        std::cout << "Version: " << tcx.getVersion() << std::endl;
-    }
-    //std::cout << opt.getId() << std::endl;
-    return 0;
+  // TcxObject tcx{false};
+  std::cout << tcx.print() << std::endl;
+  // Options opt("/Users/stratis/Desktop/dev/c++/tcx_creator/options.json");
+  if (!tcx.isEmpty()) {
+
+    std::cout << "Version: " << tcx.getVersion() << std::endl;
+  }
+  // std::cout << opt.getId() << std::endl;
+  return 0;
 }
